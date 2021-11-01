a783b02ebb431e83a71cbeea900accbbaa70f10f
thanhhoan <pdth456@gmail.com>
2021-11-01 18:56:48 +0700

    first commit 🚀

---
+++ desktop/src/com/megaman/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.megaman.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.megaman.game.Megaman;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Megaman(), config);
+	}
+}
