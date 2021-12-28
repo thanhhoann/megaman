e261d0c75179a2e7c2a47879db696de76ea6fa08
NhatMinh2910 <90759175+NhatMinh2910@users.noreply.github.com>
2021-12-28 19:59:58 +0700

    Testing start button on menuscreen

--- core/src/com/megaman_oop/megaman/Screens/MenuScreen.java
+++ core/src/com/megaman_oop/megaman/Screens/MenuScreen.java
@@ -3,17 +3,20 @@ package com.megaman_oop.megaman.Screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.megaman_oop.megaman.GifDecoder;
@@ -27,40 +30,55 @@ public class MenuScreen implements Screen {
     Texture start = new Texture(Gdx.files.internal("menu_start.png"));
     Texture rule = new Texture(Gdx.files.internal("menu_rule.png"));
     Texture quit = new Texture(Gdx.files.internal("menu_quit.png"));
+    Drawable drawableStart = new TextureRegionDrawable(new TextureRegion(start));
+    ImageButton startBtn;
     SpriteBatch batch = new SpriteBatch();
     float elapsed;
 
-    private Game game;
+    private final Game game;
 
-    public MenuScreen(Game game) {
+    public MenuScreen(final Game game) {
         this.game = game;
         viewport = new FitViewport(MegaMan.V_WIDTH, MegaMan.V_HEIGHT, new OrthographicCamera());
         stage = new Stage(viewport, ((MegaMan) game).batch);
+        Gdx.input.setInputProcessor(stage);
 
+        startBtn = new ImageButton(drawableStart, drawableStart);
+        startBtn.setPosition(75, 300);
+        startBtn.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new PlayScreen((MegaMan) game));
+                dispose();
+            }
+        });
+
+        stage.addActor(startBtn);
     }
 
     @Override
     public void show() {
-
     }
 
     @Override
     public void render(float delta) {
 
         elapsed += Gdx.graphics.getDeltaTime();
-        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClearColor(0, 0, 1, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(animation.getKeyFrame(elapsed), 0, 0);
-        batch.draw(start, 75, 300);
-        batch.draw(rule, 75, 200);
-        batch.draw(quit, 75, 100);
         batch.end();
+        stage.act();
         stage.draw();
     }
 
+
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {
+
+    }
 
     @Override
     public void pause() {
