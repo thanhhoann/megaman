63bc38cae56bded9e77ee8aaee119c3b909d0dcf
Minh Phan <miphanng@gmail.com>
2022-01-01 22:59:05 +0700

    camera function for enemy and character

--- core/src/com/megaman_oop/megaman/Screens/CameraStyles.java
+++ core/src/com/megaman_oop/megaman/Screens/CameraStyles.java
@@ -2,13 +2,15 @@ package com.megaman_oop.megaman.Screens;
 
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Vector3;
+import com.megaman_oop.megaman.MegaMan;
 import com.megaman_oop.megaman.Sprites.MainCharacter;
+import com.megaman_oop.megaman.Sprites.Enemies.Enemy;
 
 public class CameraStyles {
     public static void lerpToCharacter(Camera camera, MainCharacter character){
         Vector3 position = camera.position;
-        position.x = camera.position.x + (character.getX() - camera.position.x)*.05f;
-        position.y = camera.position.y + (character.getY() - camera.position.y)*.05f;
+        position.x = camera.position.x + (character.getX()*MegaMan.PPM - camera.position.x)*.1f;
+        position.y = camera.position.y + (character.getY()*MegaMan.PPM - camera.position.y)*.05f;
         camera.position.set(position);
         camera.update();
     }
@@ -27,6 +29,17 @@ public class CameraStyles {
         if (position.y > startY + height){
             position.y = startY + height;
         }
+        camera.position.set(position);
+        camera.update();
+    }
+    
+    public static void averageBetweenTarget(Camera camera, MainCharacter mainCharacter, Enemy enemy){
+        float avgX = (mainCharacter.getX() + enemy.getX())/2;
+        float avgY = (mainCharacter.getY() + enemy.getY())/2;
+        
+        Vector3 position = camera.position;
+        position.x = camera.position.x + (avgX- camera.position.x)*.1f;
+        position.y = camera.position.y + (avgY - camera.position.y)*.1f;
         camera.position.set(position);
         camera.update();
     }
