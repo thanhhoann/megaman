da5482c756898ac73f54ab98e4f3a64739b21822
Truong Hung Quan <61302047+TruongHungQuan@users.noreply.github.com>
2022-01-03 16:00:32 +0700

    Adding new Heart class and ItemBehaivour interface

---
+++ core/src/com/megaman_oop/megaman/Sprites/Items/Heart.java
@@ -0,0 +1,71 @@
+package com.megaman_oop.megaman.Sprites.Items;
+
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.*;
+import com.megaman_oop.megaman.MegaMan;
+import com.megaman_oop.megaman.Screens.PlayScreen;
+import com.megaman_oop.megaman.Sprites.Interface.ItemBehaivour;
+
+
+public class Heart extends Sprite implements ItemBehaivour {
+    PlayScreen screen;
+    World world;
+    boolean destroyed;
+    boolean setToDestroy;
+    float stateTime;
+    Body body;
+
+    public Heart(PlayScreen screen, float x, float y) {
+        this.screen = screen;
+        this.world = screen.getWorld();
+        setRegion(screen.getAtlas().findRegion("heart"),0,0,32,32);
+        setBounds(x, y, 10/ MegaMan.PPM, 10/ MegaMan.PPM);
+        defineItem();
+        body.setActive(false);
+    }
+
+    public void defineItem() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(getX(),getY());
+        bdef.type= BodyDef.BodyType.StaticBody;
+        body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5/ MegaMan.PPM);
+        fdef.filter.categoryBits= MegaMan.ITEM_BIT;
+        fdef.filter.maskBits = MegaMan.GROUND_BIT |
+                MegaMan.MEGAMAN_BIT
+                | MegaMan.ENEMY_BIT
+                | MegaMan.ENEMY_HEAD_BIT
+                | MegaMan.OBJECT_BIT;
+        fdef.shape= shape;
+        body.createFixture(fdef).setUserData(this);
+    }
+    public void update(float dt){
+        stateTime += dt;
+        setPosition(getX() ,getY() );
+        if((stateTime > 2 || setToDestroy) && !destroyed) {
+            world.destroyBody(body);
+            destroyed = true;
+        }
+    }
+
+    @Override
+    public void setToDestroy() {
+        setToDestroy = true;
+    }
+
+    @Override
+    public void setActive() {
+        body.setActive(true);
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+
+
+}
